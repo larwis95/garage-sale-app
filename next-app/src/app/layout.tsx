dffd572a4e3b8052941b58b201cf3e7af7b39311
Katie Bug <katierosealford@gmail.com>
2024-07-15 20:48:27 -0400

    just adding some front end comps for layout

--- next-app/src/app/layout.tsx
+++ next-app/src/app/layout.tsx
@@ -28,8 +28,8 @@ export default function RootLayout({
             </div>
           </div>
           <div>
-      <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Sign Up</a>
-    </div>
+            <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Sign Up</a>
+          </div>
         </div>
       </nav>
       <nav>
