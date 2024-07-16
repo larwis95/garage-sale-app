c31a63c1ad5ac0c8587924e3e7223152f27e9ad8
Katie Bug <katierosealford@gmail.com>
2024-07-15 21:32:22 -0400

    added links to buttons to correct where they lead you : )

--- next-app/src/app/layout.tsx
+++ next-app/src/app/layout.tsx
@@ -26,11 +26,14 @@ export default function RootLayout({
         <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
           <div className="text-sm lg:flex-grow" style={{ margin: '10px' }}>
             <div>
-              <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Login</a>
+              <a href="/login" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Login</a>
             </div>
           </div>
           <div>
-            <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Sign Up</a>
+            <a href="/" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Home</a>
+          </div>
+          <div>
+            <a href="/signup" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Sign Up</a>
           </div>
         </div>
       </nav>
