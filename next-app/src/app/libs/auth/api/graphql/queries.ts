bd259ea59e703ef30ab7c4bac5e7a5c1c612de6c
Larry Wisniewski <larryw48390@gmail.com>
2024-07-15 22:19:21 -0400

    graphQL api fleshed out

--- next-app/src/app/libs/auth/api/graphql/queries.ts
+++ next-app/src/app/libs/auth/api/graphql/queries.ts
@@ -56,3 +56,91 @@ export const GET_ME = gql`
     }
   }
 `;
+
+export const GET_SALES = gql`
+  query sales {
+    sales {
+      _id
+      title
+      description
+      items {
+        _id
+        name
+        category
+        description
+        condition
+        price
+        quantity
+        discount
+        picture
+      }
+      category
+      startDate
+      endDate
+      location
+      discount
+      recurring
+      coordinates
+    }
+  }
+`;
+
+export const GET_SALE = gql`
+  query sale($id: ID!) {
+    sale(id: $id) {
+      _id
+      title
+      description
+      items {
+        _id
+        name
+        category
+        description
+        condition
+        price
+        quantity
+        discount
+        picture
+      }
+      category
+      startDate
+      endDate
+      location
+      discount
+      recurring
+      coordinates
+    }
+  }
+`;
+
+export const GET_ITEM = gql`
+  query item($id: ID!) {
+    item(id: $id) {
+      _id
+      name
+      category
+      description
+      condition
+      price
+      quantity
+      discount
+      picture
+    }
+  }
+`;
+
+export const GET_ITEMS = gql`
+  query items {
+    items {
+      _id
+      name
+      category
+      description
+      condition
+      price
+      quantity
+      discount
+      picture
+    }
+  }
+`;
