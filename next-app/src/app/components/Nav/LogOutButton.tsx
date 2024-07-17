72c471bcbbd06cc0db14777ddae7f45efcaea3ec
Larry Wisniewski <larryw48390@gmail.com>
2024-07-17 04:58:07 -0400

    finished nav bar

--- next-app/src/app/components/Nav/LogOutButton.tsx
+++ next-app/src/app/components/Nav/LogOutButton.tsx
@@ -1,16 +1,17 @@
+"use client";
 import Auth from "@/app/libs/auth/frontend";
 import { useState, useEffect } from "react";
 
 export default function LogOutButton() {
-  const [loggedIn, setLoggedIn] = useState<boolean | undefined>(undefined);
+  const [loggedIn, setLoggedIn] = useState<boolean | undefined>(false);
   useEffect(() => {
     setLoggedIn(Auth.loggedIn());
-  }, [loggedIn]);
+  }, []);
 
   return (
     <>
       {loggedIn && (
-        <a onClick={Auth.logout} className="text-white cursor-pointer bg-black p-2 hover:text-red-500 hover:bg-slate-700 hover:border-red-700 hover:scale-105 transition duration-500 rounded-lg border border-white">
+        <a onClick={Auth.logout} className="cursor-pointer rounded-lg border border-white bg-black p-2 text-white transition duration-500 hover:scale-110 hover:border-red-700 hover:bg-slate-700 hover:text-red-500">
           Logout
         </a>
       )}
--- next-app/src/app/components/Nav/index.tsx
+++ next-app/src/app/components/Nav/index.tsx
@@ -1,7 +1,8 @@
 "use client";
 import NavLink from "./NavLink";
 import LogOutButton from "./LogOutButton";
-import Auth from "@/app/libs/auth/frontend";
+import { useState, useEffect, useRef } from "react";
+import HamburgerMenu from "./HamburgerMenu";
 
 const Links = [
   { href: "/", text: "Home" },
@@ -12,20 +13,61 @@ const Links = [
 ];
 
 export default function Nav() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isOpened, setIsOpened] = useState(false);
+  const navRef = useRef(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (navRef.current && !(navRef.current as HTMLElement).contains(event.target as Node)) {
+      setIsOpened(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY >= 60);
+    };
+    window.addEventListener("scroll", handleScroll);
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <header className="absolute top-5">
-      <nav>
-        <ul className="flex flex-row gap-2 justify-center items-center">
-          {Links.map((link, index) => (
-            <li key={index}>
-              <NavLink href={link.href} loggedInOnly={link.loggedInOnly} loggedOutOnly={link.loggedOutOnly} text={link.text} />
+    <header className={`${isScrolled ? "fixed top-5" : "absolute left-4"} top-5 z-50`}>
+      {!isScrolled && (
+        <nav>
+          <ul className="flex flex-row items-center justify-center gap-2">
+            {Links.map((link, index) => (
+              <li key={index}>
+                <NavLink href={link.href} loggedInOnly={link.loggedInOnly} loggedOutOnly={link.loggedOutOnly} text={link.text} />
+              </li>
+            ))}
+            <li>
+              <LogOutButton />
             </li>
-          ))}
-          <li>
-            <LogOutButton />
-          </li>
-        </ul>
-      </nav>
+          </ul>
+        </nav>
+      )}
+      {isScrolled && (
+        <nav className="relative z-50 flex h-fit w-fit flex-col items-start gap-2" ref={navRef}>
+          {!isOpened && (
+            <button onClick={() => setIsOpened(!isOpened)} className="ml-3 mt-4 flex h-8 w-8 flex-col items-center justify-center rounded-full border border-white bg-black p-2 text-white transition duration-500 hover:scale-110 hover:border-red-700 hover:bg-slate-700 hover:text-red-500">
+              ☰
+            </button>
+          )}
+          {isOpened && (
+            <>
+              <button onClick={() => setIsOpened(!isOpened)} className="z-50 ml-3 mt-4 flex h-8 w-8 items-center justify-center rounded-full border border-white bg-black p-2 text-white transition duration-500 hover:scale-110 hover:border-red-700 hover:bg-slate-700 hover:text-red-500">
+                X
+              </button>
+              <HamburgerMenu />
+            </>
+          )}
+        </nav>
+      )}
     </header>
   );
 }
