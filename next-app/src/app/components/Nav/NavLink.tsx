9ef56b92965b39f5634ac0b0b6be0c601af3f536
Larry Wisniewski <larryw48390@gmail.com>
2024-07-17 04:58:07 -0400

    finished nav bar

--- next-app/src/app/components/Nav/NavLink.tsx
+++ next-app/src/app/components/Nav/NavLink.tsx
@@ -1,7 +1,8 @@
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Auth from "@/app/libs/auth/frontend";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
 interface INavLink {
   href: string;
@@ -11,27 +12,27 @@ interface INavLink {
 }
 
 export default function NavLink({ href, loggedInOnly, loggedOutOnly, text }: INavLink) {
-  const [loggedIn, setLoggedIn] = useState<boolean | undefined>(undefined);
+  const [loggedIn, setLoggedIn] = useState<boolean | undefined>(false);
   const pathname = usePathname();
   useEffect(() => {
     setLoggedIn(Auth.loggedIn());
-  }, [loggedIn]);
+  }, []);
 
   return (
     <>
       {loggedInOnly && loggedIn && (
-        <Link href={href} className={`${pathname === href ? "text-red-600 bg-black pointer-events-none" : "text-white bg-black"} p-2 hover:text-red-500 hover:bg-slate-700 hover:border-red-700 hover:scale-105 transition duration-500 rounded-lg border border-white`}>
+        <Link href={href} className={`${pathname === href ? "pointer-events-none bg-black text-red-600" : "bg-black text-white"} rounded-lg border border-white p-2 transition duration-500 hover:scale-105 hover:border-red-700 hover:bg-slate-700 hover:text-red-500`}>
           {text}
         </Link>
       )}
 
       {loggedOutOnly && !loggedIn && (
-        <Link href={href} className={`${pathname === href ? "text-red-600 bg-black pointer-events-none" : "text-white bg-black"} p-2 hover:text-red-500 hover:bg-slate-700 hover:scale-105 hover:border-red-700 transition duration-500 rounded-lg border border-white`}>
+        <Link href={href} className={`${pathname === href ? "pointer-events-none bg-black text-red-600" : "bg-black text-white"} rounded-lg border border-white p-2 transition duration-500 hover:scale-105 hover:border-red-700 hover:bg-slate-700 hover:text-red-500`}>
           {text}
         </Link>
       )}
       {loggedInOnly === undefined && loggedOutOnly === undefined && (
-        <Link href={href} className={`${pathname === href ? "text-red-600 bg-black pointer-events-none" : "text-white bg-black"} p-2 hover:text-red-500 hover:bg-slate-700 hover:scale-105 hover:border-red-700 transition duration-500 rounded-lg border border-white`}>
+        <Link href={href} className={`${pathname === href ? "pointer-events-none bg-black text-red-600" : "bg-black text-white"} rounded-lg border border-white p-2 transition duration-500 hover:scale-105 hover:border-red-700 hover:bg-slate-700 hover:text-red-500`}>
           {text}
         </Link>
       )}
