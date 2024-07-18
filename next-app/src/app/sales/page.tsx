f6c73c64d856b73dc9e4f859623ded0967c5ccdb
Katie Bug <katierosealford@gmail.com>
2024-07-18 18:39:31 -0400

    changed text color on homepage

--- next-app/src/app/sales/page.tsx
+++ next-app/src/app/sales/page.tsx
@@ -1,11 +1,15 @@
 import useUserLocation from "../hooks/useUserLocation";
 import SaleSection from "../components/SaleSection";
+import { Sale } from "@/app/components/lottie";
 
 export default async function Sales() {
   const { latitude, longitude } = await useUserLocation();
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-around p-10">
+    {/* <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Sale />
+    </div> */}
+      <main className="flex min-h-screen flex-col items-center  justify-around p-10">
         <SaleSection coordinates={{ latitude, longitude }} />
       </main>
     </>
