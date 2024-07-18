c73a25021bdbc69fd1a33f10f9410b6c8a666091
FonKnp <fonnfonk@gmail.com>
2024-07-17 21:52:12 -0400

    add div in login signup and profile page

--- next-app/src/app/login/page.tsx
+++ next-app/src/app/login/page.tsx
@@ -30,6 +30,7 @@ export default function Login() {
   };
   return (
     <div className="h-screen">
+    <div className="container py-20">
     <div className="text-center">
       <h2 className="font-extrabold text-white">LOGIN</h2>
     </div>
@@ -54,6 +55,7 @@ export default function Login() {
       </div>
       <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="submit">Submit</button>
     </form>
+    </div>
     </div>
   )
 }
