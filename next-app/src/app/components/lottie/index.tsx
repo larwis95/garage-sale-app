897b71791b38d0ca42c47e4279e19762f902b57f
Katie Bug <katierosealford@gmail.com>
2024-07-18 18:18:35 -0400

    changed design style

--- next-app/src/app/components/lottie/index.tsx
+++ next-app/src/app/components/lottie/index.tsx
@@ -3,6 +3,7 @@ import Lottie from "lottie-react";
 import treasure from "@/app/images/treasure.json";
 import sale2 from "@/app/images/sale2.json";
 import spotter1 from "@/app/images/spotter1.json";
+import treasureMap from "@/app/images/treasureMap.json";
 
 export function Treasure() {
   return (
@@ -24,6 +25,14 @@ export function Spotter() {
   return (
     <div className="lottie-icon inline-block w-60 h-60">
       <Lottie loop={true} animationData={spotter1}></Lottie>
+    </div>
+  );
+}
+
+export function TreasureMap() {
+  return (
+    <div className="lottie-icon inline-block w-48 h-48">
+      <Lottie loop={true} animationData={treasureMap}></Lottie>
     </div>
   );
 }
