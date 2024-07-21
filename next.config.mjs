0ef06735c8e1986279a566602454cc27df1a3dd6
Larry Wisniewski <larryw48390@gmail.com>
2024-07-21 04:56:30 -0400

    test

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,14 @@
 
 const nextConfig = {
   async rewrites() {
-    if (process.env.NODE_ENV === "production")
+    if (process.env.NODE_ENV === "production") {
       return [
         {
           source: `/api/graphql`,
-          destination: `${process.env.VERCEL_URL}/api/graphql`,
+          destination: `http://${process.env.VERCEL_URL}/api/graphql`,
         },
       ];
+    }
     return [
       {
         source: `/api/graphql`,
