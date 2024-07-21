978724cd45d2e0281c0a960c3988ad96e388be39
FonKnp <fonnfonk@gmail.com>
2024-07-21 02:09:58 -0400

    change and add color

--- next-app/src/app/profile/page.tsx
+++ next-app/src/app/profile/page.tsx
@@ -1,9 +1,11 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useQuery, useMutation } from "@apollo/client";
 import { GET_ME } from "../libs/auth/api/graphql/queries";
 import { ADD_SALE, UPDATE_SALE, DELETE_SALE, DELETE_FAVORITE } from "../libs/auth/api/graphql/mutations";
 import { format } from "date-fns";
+import Link from "next/link";
+import { Notification } from "../providers/Notification";
 import { motion } from "framer-motion";
 
 interface ISale {
@@ -40,7 +42,7 @@ export default function Profile() {
   const { data, loading } = useQuery(GET_ME);
   const userSales = data?.me?.sales || [];
   const [formState, setFormState] = useState<IFormState>({ saleId: null, title: "" });
-  
+  const { setNotification } = useContext(Notification);
 
   const [addSale] = useMutation(ADD_SALE, {
     refetchQueries: [{ query: GET_ME }],
@@ -64,30 +66,43 @@ export default function Profile() {
 
   if (loading) return <p className="h-screen py-20 text-center">Loading...</p>;
 
+
+
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log(formState);
-    if (formState.saleId) {
-      await updateSale({ variables: { ...formState } });
-    } else if (formState.saleId) {
-      await addSale({ variables: {...formState} });
-    } else {
-      await deleteFavorite({ variables: {...formState} });
+    try {
+      if (formState.saleId) {
+        const { data } = await updateSale({ variables: { ...formState } });
+        if (data.errors) {
+          throw new Error('Error updating sale!');
+        } 
+        setNotification({ type: "success", message: "Sale update success!" })
+      } else {
+        const { data } = await addSale({ variables: {...formState} });
+        if (data.errors) {
+          throw new Error('Error adding sale!');
+        }
+        setNotification({ type: "success", message: "Sale added success!" })
+      } 
+      setFormState({ title: "", description: "", saleId: null });
+    } catch(err: any) {
+      setNotification({ type: "error", message: err.message })
     }
-    setFormState({ title: "", description: "", saleId: null });
-  };
+    };
+
 
   return (
     <div className="h-screen w-screen">
       <div className="py-20">
-        <div className="mx-14 text-center max-h-fit min-h-fit w-11/12 rounded-lg border border-teal-500 bg-slate-600 p-4">
+        <div className="mx-14 text-center max-h-fit min-h-fit w-11/12 rounded-lg border border-teal-500 bg-slate-800 p-4">
           <h2 className="text-3xl font-bold text-yellow-300">{data.me.username} profile</h2>
           <p className="py-2 text-center text-xl font-bold text-yellow-300">Your Email: {data.me.email}</p>
         </div>
         {/* Sales and Favorite container */}
         <div className="m-5 flex flex-row">
           {/* Sales container */}
-          <div className="m-1 max-h-fit min-h-fit w-1/2 rounded-lg border border-teal-500 bg-slate-600 p-4">
+          <div className="m-1 max-h-fit min-h-fit w-1/2 rounded-lg border border-teal-500 bg-slate-800 p-4">
             <p className="m-2 p-2 text-xl font-bold text-yellow-300">Your Sales: </p>
             {userSales.map((sale: ISale) => (
               <div key={sale._id} className="m-1 max-h-fit min-h-fit rounded-lg border border-teal-500 bg-slate-700 p-2">
@@ -96,6 +111,7 @@ export default function Profile() {
                 <p className="py-1 text-white">Start-date: {format(new Date(Number(sale.startDate)), "MM/dd/yy")}</p>
                 <p className="py-1 text-white">End-date: {format(new Date(Number(sale.endDate)), "MM/dd/yy")}</p>
                 <p className="py-1 text-white">Location: {sale.location}</p>
+                <Link className="" href={`/sales/${sale._id}`}>View Sale</Link>
                 <div className="flex justify-end">
                   <button
                     className="m-1 p-2 rounded bg-blue-500 hover:bg-blue-600"
@@ -113,19 +129,23 @@ export default function Profile() {
             ))}
           </div>
           {/* favorite container */}
-          <div className="m-1 max-h-fit min-h-fit w-1/2 rounded-lg border border-teal-500 bg-slate-600 p-4">
+          <div className="m-1 max-h-fit min-h-fit w-1/2 rounded-lg border border-teal-500 bg-slate-800 p-4">
             <p className="m-2 p-2 text-xl font-bold text-yellow-300">Favorites: </p>
             {data.me.favorites.map((favorite: iFavorite) => (
               <div key={favorite._id} className="m-1 max-h-fit min-h-fit rounded-lg border border-teal-500 bg-slate-700 p-2">
                 <h3>{favorite.title}</h3>
                 <p>{favorite.description}</p>
+                <Link className="" href={`/sales/${favorite._id}`}>View Favorite</Link>
+                <button
+                  className="m-1 rounded bg-red-500 p-1 hover:bg-red-600"
+                  onClick={async () => await deleteFavorite({ variables: { _id: favorite._id} })}> Delete </button>
               </div>
             ))}
           </div>
         </div>
         {/* add edit sales container */}
         <div className="flex justify-center">
-          <div className="w-1/2 rounded-lg border border-teal-500 bg-slate-600 p-4">
+          <div className="w-1/2 rounded-lg border border-teal-500 bg-slate-800 p-4">
             <form onSubmit={handleSubmit} className="flex flex-col m-2">
               <p className="py-2 text-xl text-white">{formState.saleId ? "Edit Sale:" : "Add Sale:"} </p>
               {/* Title input */}
@@ -141,11 +161,11 @@ export default function Profile() {
                 placeholder=" Start Date" required
                 className="py-2 m-2 border text-black rounded" />
               {/* End date Input */}
-              <input type="date " value={formState.endDate} onChange={(e) => setFormState({ ...formState, endDate: e.target.value })}
+              <input type="date" value={formState.endDate} onChange={(e) => setFormState({ ...formState, endDate: e.target.value })}
                 placeholder=" End Date" required
                 className="py-2 m-2 border text-black rounded" />
               {/* Location Input */}
-              <input type="text " value={formState.location} onChange={(e) => setFormState({ ...formState, location: e.target.value })}
+              <input type="text" value={formState.location} onChange={(e) => setFormState({ ...formState, location: e.target.value })}
                 placeholder=" Location" required
                 className="py-2 m-2 border text-black rounded" />
               <button type="submit" className="m-2 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">
