3b1cf2d8b4e4aed0909128f4018270b5744111a9
Larry Wisniewski <larryw48390@gmail.com>
2024-07-22 03:30:36 -0400

    ip change again

--- src/app/hooks/useUserLocation.tsx
+++ src/app/hooks/useUserLocation.tsx
@@ -10,10 +10,11 @@ interface IUserLocation {
 export default async function useUserLocation(): Promise<IUserLocation> {
   const header = headers();
   const ip = header.get("x-forwarded-for") || header.get("x-real-ip");
+
   const client = getClient();
   const { data } = await client.query({
     query: GET_USER_LOCATION,
-    variables: { ip: ip === "::1" || "127.0.0.1" ? "68.37.253.45" : ip },
+    variables: { ip: process.env.NODE_ENV === "production" ? ip : "68.37.253.45" },
   });
   const { latitude, longitude } = data.userLocation;
   return { latitude, longitude };
