2381182bce43b3cea3c2ac6cc2b426502239e9cd
FonKnp <fonnfonk@gmail.com>
2024-07-22 21:52:22 -0400

    edit sale section and hamburger and home page

--- src/app/components/Cards/Sales/header.tsx
+++ src/app/components/Cards/Sales/header.tsx
@@ -13,9 +13,9 @@ export default function SalesHeader({ title, startDate, endDate }: ISalesCardHea
   const formattedEndDate = format(endDate, "MM/dd/yyyy");
 
   return (
-    <div className="top-0 flex w-full flex-grow flex-col border-b border-red-600 p-2 text-left">
+    <div className="flex flex-col border-b border-teal-500 p-2 ">
       <h2 className="text-2xl font-bold">{title}</h2>
-      <small className="text-sm text-gray-500">
+      <small className="text-sm text-gray-300">
         {formattedStartDate} - {formattedEndDate}
       </small>
     </div>
