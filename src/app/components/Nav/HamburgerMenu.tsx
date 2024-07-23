5babcce229238dbbb8bd366bad1b72f232f9b24e
Katie Bug <katierosealford@gmail.com>
2024-07-22 20:18:44 -0400

    updated hamburger menu color to make it stand out more, added some text under map on home page but commented out until team approves

--- src/app/components/Nav/HamburgerMenu.tsx
+++ src/app/components/Nav/HamburgerMenu.tsx
@@ -4,13 +4,13 @@ import { motion } from "framer-motion";
 
 export default function HamburgerMenu() {
   return (
-    <motion.div className={`h-50svh w-50svw absolute flex flex-col justify-start gap-2 rounded-r-lg border-b border-r border-t border-yellow-300 bg-slate-700 bg-opacity-65 p-2 align-middle backdrop-blur-sm`} initial={{ x: "-100vw" }} animate={{ x: 0 }} exit={{ x: "-100vw" }} transition={{ duration: 0.5 }}>
+    <motion.div className={`h-50svh w-50svw absolute flex flex-col justify-start gap-2 rounded-r-lg border-b border-r border-t border-yellow-300 bg-teal-200 bg-opacity-65 p-2 align-middle backdrop-blur-sm`} initial={{ x: "-100vw" }} animate={{ x: 0 }} exit={{ x: "-100vw" }} transition={{ duration: 0.5 }}>
       <ul className="mt-12 flex flex-col">
         <HamburgerMenuLink href="/" text="Home" />
         <HamburgerMenuLink href="/sales" text="Sales" />
         <HamburgerMenuLink href="/profile" text="Profile" loggedInOnly />
         <HamburgerMenuLink href="/login" text="Login" loggedOutOnly />
-        <HamburgerMenuLink href="/signup" text="Signup" loggedOutOnly />
+        <HamburgerMenuLink href="/signup" text="Sign Up" loggedOutOnly />
         <HamburgerMenuLogout />
       </ul>
     </motion.div>
